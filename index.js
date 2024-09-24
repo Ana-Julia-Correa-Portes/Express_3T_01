82ceffc52af93852cac42319111ecb4a81d94e30
Correa <ana.julia.correa.portes@gmail.com>
2024-09-24 11:25:55 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+// CRIAÇÃO DA PORTA
+const PORT = 3333;
+
+// CRIAÇÃO DA PORTA HOMEPAGE
+app.get("/", (req, res) => {
+  res.send("Olá, página inicial!");
+});
+
+//  ESCUTANDO UMA PORTA DO SERVIDOR
+app.listen(PORT, function () {
+  console.log(" ESCUTANDO A PORTA " + PORT);
+});
